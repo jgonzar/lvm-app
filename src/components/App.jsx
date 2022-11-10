a669b5d2510a914a03f094141d2250de729b25c1
Joseito <angel.gonzalez@stackitgroup.com>
2022-11-10 10:59:31 -0400

    starting files

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+const App = () => {
+    return (
+      <>
+        <h1>Hello world</h1>
+      </>  
+    );
+};
+
+export {App};
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {App} from '@components/App';
+
+const root = document.getElementById('root');
+
+root.render(<App />);
