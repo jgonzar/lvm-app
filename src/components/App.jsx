9289765c091be24b767947a89d6bfe409234c133
Joseito <angel.gonzalez@stackitgroup.com>
2022-11-10 11:22:38 -0400

    oh oh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,9 @@ import react from "react";
 
 const App = () => {
     return (
-      <>
+      <React.Fragment>
         <h1>Hello world</h1>
-      </>  
+      </React.Fragment>  
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {App} from '@components/App';
+import {createRoot} from 'react-dom/client';
+import {App} from '../src/components/App';
 
-const root = document.getElementById('root');
+const container = document.getElementById('app');
+const root = createRoot(container);
 
 root.render(<App />);
